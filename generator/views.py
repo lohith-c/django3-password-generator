114bb32abe2cc85ffdc6b4dd041fbcc6333773a3
Lohith C <lohithc73@gmail.com>
2022-06-08 00:30:03 +0530

    First commit

---
+++ generator/views.py
@@ -0,0 +1,37 @@
+from ssl import HAS_TLSv1_1
+from xml.dom.minidom import CharacterData
+from django import http
+from django.shortcuts import render
+from django.http import HttpResponse
+import random
+
+# Create your views here.
+def home(request):
+    return render(request, 'generator/home.html')
+
+def password(request):
+    
+    characters = list('abcdefghijklmnopqrstuvwxyz')
+
+    if request.GET.get('uppercase'):
+        characters.extend('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+
+    if request.GET.get('numbers'):
+        characters.extend('0123456789')
+
+    if request.GET.get('special'):
+        characters.extend('!@#~$%^&*+')    
+        
+
+    thepassword = ''
+    length = int(request.GET.get('length',12))
+
+    for x in range(length):
+        thepassword += random.choice(characters)
+
+
+    return render(request, 'generator/password.html',{'password':thepassword})
+
+def about(request):
+    return render(request, 'generator/about.html')
+    
