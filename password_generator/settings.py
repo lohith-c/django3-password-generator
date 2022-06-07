d001028db988fe22118a20b104db2a7fa531ba22
Lohith C <lohithc73@gmail.com>
2022-06-08 00:34:53 +0530

    Second commit

--- password_generator/settings.py
+++ password_generator/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d2d6090(x0@rbnv7s=$pa1ao0&xlhv%fy02kgzwdd_3(xr(5pe'
+SECRET_KEY = 'django-insecure-d2d6090(x0@xasxasxxasxrbnv7s=$pa1ao0&xlhv%fy02kgzwdd_3(xr(5pe'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
